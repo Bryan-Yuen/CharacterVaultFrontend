bbd9d0cfd2598dd099540cd70e4656b68cbbd5d6
Bryan <bryanyuen1998@gmail.com>
2024-10-11 15:49:49 -0400

    settings update

--- src/apolloClient/apolloClient.tsx
+++ src/apolloClient/apolloClient.tsx
@@ -1,10 +1,11 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import 'dotenv/config';
 
+console.log("in client",process.env.NEXT_PUBLIC_PRODUCTION)
 const client = new ApolloClient({
     //uri: "http://localhost:4000",
     // this allows you to call service on other devices not on the same one
-    uri: process.env.PRODUCTION ? "https://myfapsheettestingwebsite.us/api" : "http://192.168.0.208:4000",
+    uri: process.env.NEXT_PUBLIC_PRODUCTION ? "https://myfapsheettestingwebsite.us/api" : "http://192.168.0.208:4000",
     cache: new InMemoryCache({
       typePolicies: {
         PornstarWithTags: {

b386a833edbc5a5504a15fba44b22e3f0951bfcb
Bryan <bryanyuen1998@gmail.com>
2024-12-30 15:29:15 -0500

    test without google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,12 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ChildrenWithProvider from "./ChildrenWithProvider";
-import { GoogleAnalytics, GoogleTagManager  } from "@next/third-parties/google";
+import { GoogleTagManager } from "@next/third-parties/google";
 
-/*
-if (!process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID && process.env.NEXT_PUBLIC_ENVIRONMENT === "PRODUCTION") {
-  throw new Error("no production google analytics id in production");
-}
-  */
-
-if (!process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID && process.env.NEXT_PUBLIC_ENVIRONMENT === "PRODUCTION") {
+if (
+  !process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID &&
+  process.env.NEXT_PUBLIC_ENVIRONMENT === "PRODUCTION"
+) {
   throw new Error("no production google tag manager id in production");
 }
 
@@ -28,11 +25,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID || ""} />
+      {/*      <GoogleTagManager
+        gtmId={process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID || ""}
+      />*/}
       <body className={inter.className}>
         <ChildrenWithProvider>{children}</ChildrenWithProvider>
       </body>
-      {/*<GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || ""} />*/ }
     </html>
   );
 }

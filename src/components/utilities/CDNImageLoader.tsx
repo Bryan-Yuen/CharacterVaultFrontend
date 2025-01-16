3ccb0c4afd0b49e11629f8b4dcf7f5cd3e37d0c1
Bryan <bryanyuen1998@gmail.com>
2025-01-15 19:00:11 -0500

    update image loader

--- src/components/utilities/CDNImageLoader.tsx
+++ src/components/utilities/CDNImageLoader.tsx
@@ -2,10 +2,10 @@ if (!process.env.NEXT_PUBLIC_BUNNY_CDN_URL) {
   throw new Error("no bunny cdn url");
 }
 
-export const CDNImageLoader = ({ src, width }: any) => {
+export const CDNImageLoader = ({ src }: any) => {
   // returns the based r2 url because the cdn might not have time to grab the picture from r2
   const imagePath = src.split("/").pop();
   const optimizedCdnImageUrl =
-    process.env.NEXT_PUBLIC_BUNNY_CDN_URL + imagePath + "?width=" + width;
+    process.env.NEXT_PUBLIC_BUNNY_CDN_URL + imagePath + "?width=" + 640;
   return optimizedCdnImageUrl;
 };
